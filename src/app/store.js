e21f7ac0dfbfd60efe26e19445880e08341de29a
Ravi <chauhan.ravi100891@gmail.com>
2023-07-10 15:50:18 +0530

    added the cartSidebar functionality

--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import wishCartReducer from "./wishCartSlice";
 
 const store = configureStore({
-   reducer: {
-      wishCartState: wishCartReducer,
-   },
+	reducer: {
+		wishCartState: wishCartReducer,
+	},
 });
 
 export default store;
